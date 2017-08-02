5b1e9be5e15b4ed4e275330505003b5f4047c031
luciousASC4 <lucious.lokko@scholar.democracyprep.org>
2017-08-02 08:44:24 -0700

    lucious-nestedapis

---
+++ lucious-nestedapis/nested.js
@@ -0,0 +1,19 @@
+ $.ajax({
+  url: 'https://randomuser.me/api/',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    var userFirstname= data.results[0].name.first
+    console.log(data.results[0].name.first)
+       $("body").append(userFirstname+" ");
+     var userLastname= data.results[0].name.last
+     console.log(userLastname);
+      $("body").append(userLastname+" ");
+       var userPic = (data.results[0].picture.large);
+    console.log(userPic);
+     $("body").append(userPic+" ");
+    
+
+  }
+}); 
+
--- lucious-userapi/ajax.js
+++ lucious-userapi/ajax.js
@@ -5,12 +5,22 @@ $.ajax({
   url: 'https://randomuser.me/api/',
   dataType: 'json',
   success: function(data) {
-    //console.log(data.results);
-    var name = data.results[0]
-    console.log(name);
-    name.name
-    console.log(name.name);
-    var first = name.name.first
-    console.log(first);
+   var userFirstname= data.results[0].name.first
+   //console.log(userFirstname);
+   $("body").append(userFirstname+" ");
+   var userLastname= data.results[0].name.last
+   console.log(data.results[0].name);
+    console.log(userLastname+" ");
+   console.log(data.results[0]);
+    $("body").append(userLastname);
+    var userPic = (data.results[0].picture.large);
+    $("body").append(userPic+" ");
+    var locat = (data.results[0].location.state);
+    $("body").append(locat+ " ");
+
+
+   
+  
+
   }
 });
